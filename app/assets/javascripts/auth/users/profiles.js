4105883ac034561ef0b36e249cba0ceb1eaccf64
bhargav <bhargav.r.raut@gmail.com>
2017-04-15 18:55:36 +0530

    updates images on clicking next, prev, move onto showing last few activities

--- app/assets/javascripts/auth/users/profiles.js
+++ app/assets/javascripts/auth/users/profiles.js
@@ -1,29 +1,85 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-$(document).on("click","#get_activities",function(event){
-	
-	
-});
+/***
+HOW STUFF WORKS.
+There are two scenarios that have to be covered.
+a. the current month that loads when the page opens: 
+	- for this purpose we have hooked into the basic constructor of the calendario object
+	- there was an option inside calendario.js, in the _init function, inside which is called a _generateTemplate function. INside that function there is an option to call a callback function at the end of the _generateTemplate. 
+	- so I have defined a custom function here in this file, and passed it in the options of the calendario instance, labelled as "callback".
+	- this is then passed to the _generateTemplate function and called at the end of that function.
+	- the callback defined in this file is "load_current_month".
+b. the months that load when we click , prev/next
+	- here we have hooked into the function called updateMonthYear.
+	- here we just get the current_month and year, from calendario itself and use that to calculate the start moment and end moment.
+
+Common pathway for both functions in the end is as follows:
+call get_activities(callback, start_moment, end_moment)
+->
+this makes the ajax calls to the server, and returns the data to the callback function mentioned in the get_activities function call.
+->
+the callback defined in this file is "update_calendar_images_and_popups", which basically uses the data got back to update the individual date cells, based on the data-attribute, which we added in the calendario.js file.
+->end.
+***/
 
 /***
-@returns : ajax_promise
+WORDJELLY FUNCTION
+@param done_function[fn] : the function to be called after the data is done loading.
+@param start_moment[Moment] : a Moment.js object reflecting the start_point from when to get the activities
+@param end_moment[Moment] : a Moment.js object reflecting the end_point upto which to get the activities.
+@returns : the result of the $.get call.
 ***/
-var get_activities = function(done_function){
-	var now = moment();
-	var now_start = now.unix();
-	var startDate = now.startOf("month");
+var get_activities = function(done_function,start_moment,end_moment){
+	//if these are null then we will get inbuilt defaults from the server.
 	$.get( "/activities/get_activities",
-	 	 { user_id: $("#user_data").data("resource").id, range: {from: startDate.unix(), to: now_start} , only:["image_url"]},
+	 	 { user_id: $("#user_data").data("resource").id, range: {from: start_moment.unix(), to: end_moment.unix()} , only:["image_url"]},
 	 	 done_function,
 	 	 "json"
 	 	 );
 }
 
 /***
-logic:
-call the getactivities function after loading the calendar
-on its done call, update the cell rows as needed.
+WORDJELLY FUNCTION
+@param data[Object] : object returned from server, from the get_activities call
+@return: nil
 ***/
+var update_calendar_images_and_popups = function(data){
+	for(epoch in data){
+		var dateStr = moment.utc(parseInt(epoch)*1000).format('MM-DD-YYYY');
+		//get that particular datestr data-attribute
+		var dateDiv = $("[data-strdate='" + dateStr +"']")[0];
+		var activity = data[epoch];
+		//now update the dateDiv as needed.	
+		$(dateDiv).html("<span class='helper'></span><img class='calendar_day_background_image' src='" + activity["image_url"] + "'/>");
+	}
+}
+
+/***
+pads the given number to the required number of digits.
+@used_in : #updateMonthYear, to get the month number , correctly padded.
+@param number[Integer] : the number we have 
+@param digits[Integer] : required number of digits to pad it to.
+@return[Integer] : the @number padded with zero's to get the number of @digits
+***/
+var padDigits = function(number, digits) {
+    return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
+}
+
+
+/***
+called inside the jquery.calendario.js file.
+calendar instance is passed into it, so that you can successfully determine the start date and end date.
+whenever we do something.call , the current global object is passed in as "this" as the default argument.
+so current_month_load is registered as "callback" property in the calendario instance, that was initialized above, and in the generate_template function inside calendario.js, it is passed in as the callback, and "called", so by default the calendario instance is passed to it(current_month_load function), and we use it to determine the start_momeht and end_moment.
+@used_in : $calendario.calendario , below in this file, the basic constructor for the calendar object. This is used to populate the data for the month that loads by default on the page.
+@param Calendario[Object] : the calendario instance
+@return nil.
+***/
+var current_month_load = function(){
+	var base_moment = moment(this.today);
+	var start_moment = base_moment.startOf("month");
+	var end_moment = base_moment.endOf("month");
+	get_activities(update_calendar_images_and_popups,start_moment,end_moment);
+}
+
 $(document).ready(function(){
 
 	var transEndEventNames = {
@@ -45,41 +101,34 @@ $(document).ready(function(){
 
 			},
 			caldata : codropsEvents,
-			displayWeekAbbr : true
+			displayWeekAbbr : true,
+			callback: current_month_load
 	} ),
 
 	$month = $( '#custom-month' ).html( cal.getMonthName() ),
 	$year = $( '#custom-year' ).html( cal.getYear() );
 	$( '#custom-next' ).on( 'click', function() {
 		cal.gotoNextMonth( updateMonthYear );
-		//now run the get_activities_again.
-		//but it has to be able to use the month requried, and not just any month
-
 	} );
 	$( '#custom-prev' ).on( 'click', function() {
 		cal.gotoPreviousMonth( updateMonthYear );
 	} );
 
-	get_activities(function(data){
-		console.log("the data is:");
-		console.log(data);
-		for(epoch in data){
-			var dateStr = moment.utc(parseInt(epoch)*1000).format('MM-DD-YYYY');
-			//get that particular datestr data-attribute
-			var dateDiv = $("[data-strdate='" + dateStr +"']")[0];
-			var activity = data[epoch];
-			//now update the dateDiv as needed.	
-			console.log("the activity image url is:" + activity["image_url"]);
-			//$(dateDiv).css('background-image', 'url(' + activity["image_url"] + ')');
-			$(dateDiv).html("<span class='helper'></span><img class='calendar_day_background_image' src='" + activity["image_url"] + "'/>");
-		}
-	});
-
+	
 
 
 	function updateMonthYear() {				
 		$month.html( cal.getMonthName() );
 		$year.html( cal.getYear() );
+		/****
+		from this point it is custom , for Wordjelly purpose.
+		****/
+		var month = cal.getMonth(),
+		    year = cal.getYear(),
+		    day = "01";
+		var start_moment = moment(year.toString() + "-" + padDigits(month,2).toString() + "-" + day);
+		var end_moment = (moment(year.toString() + "-" + padDigits(month,2).toString() + "-" + day)).endOf("month");
+		get_activities(update_calendar_images_and_popups,start_moment,end_moment);
 	}
 
 	// just an example..
