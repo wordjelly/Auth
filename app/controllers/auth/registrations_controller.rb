48c1c00d2bf9d1a4203b05dc0f74bb3b0bdf67d9
bhargav <bhargav.r.raut@gmail.com>
2017-07-12 15:13:27 +0530

    able to modify form on submit, before it goes to the server

--- app/controllers/auth/registrations_controller.rb
+++ app/controllers/auth/registrations_controller.rb
@@ -1,4 +1,12 @@
 class Auth::RegistrationsController < Devise::RegistrationsController
   
+  after_action :check_resource_errors
+
+  private
+	def check_resource_errors
+		puts resource.attributes.to_s
+		true
+	end
+  
 
 end
