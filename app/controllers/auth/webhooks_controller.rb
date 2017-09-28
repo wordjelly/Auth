98889ebca210a15d7bcb77b6921a0a7b91c307b4
bhargav <bhargav.r.raut@gmail.com>
2017-09-28 16:35:33 +0530

    webhooks return 200 ok, tested with mailgun and 2factor

--- app/controllers/auth/webhooks_controller.rb
+++ app/controllers/auth/webhooks_controller.rb
@@ -1,13 +1,3 @@
 class Auth::WebhooksController <  ApplicationController
-	skip_before_filter :verify_authenticity_token
-	
-	def sms_webhook
-
-	end
-
-	def email_webhook
-
-	end
-
-	
+	skip_before_filter :verify_authenticity_token	
 end
--- spec/dummy/app/controllers/webhooks_controller.rb
+++ spec/dummy/app/controllers/webhooks_controller.rb
@@ -2,10 +2,12 @@ class WebhooksController < Auth::WebhooksController
 	def sms_webhook 
 		## send to a background job in the respective module.
 		OtpJob.perform_later([nil,nil,"sms_webhook",JSON.generate(params)])
+		render :nothing => true, :status => 200
 	end
 
 	def email_webhook
 		## send to a background job in the respective module.
 		OtpJob.perform_later([nil,nil,"email_webhook",JSON.generate(params)])
+		render :nothing => true, :status => 200
 	end
 end
