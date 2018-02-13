778c6e3dfb05777023c0c078036e4617f84d063d
bhargav <bhargav.r.raut@gmail.com>
2018-02-13 16:18:41 +0530

    admin user account creation works, now add json specs

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -40,7 +40,7 @@ class User < Auth::User
 
       super(otp)
       ## will need to pass the request_send_password_link here. as well.
-      OtpJob.perform_later([self.class.name.to_s,self.id.to_s,"verify_sms_otp",JSON.generate({:otp => otp,:request_send_reset_password_link => self.request_send_reset_password_link})])
+      OtpJob.perform_later([self.class.name.to_s,self.id.to_s,"verify_sms_otp",JSON.generate({:otp => otp})])
       
   end
 
